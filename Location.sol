b9878545b3a55159aa4be01c3c9ca6f649b72a9a
Nidhee Kamble <38733826+nidheekamble@users.noreply.github.com>
2020-08-15 19:19:40 +0530

    Location gives warning; no errors on compilation

--- Location.sol
+++ Location.sol
@@ -5,11 +5,19 @@ pragma solidity >=0.4.22 <0.7.0;
  * @dev Trigger update upon reaching location
  */
 contract Location {
-
+    
+    uint256 lat;
+    uint256 long;
+    
+    //constructor defines location values
+    constructor (uint256 _lat, uint256 _long) public {
+        lat = _lat;
+        long = _long;
+    }
+    
     // the message received by the contract is in 'currentStatus'
-
-    function updateStatus(bool currentStatus) public returns (string) {
-        if currentStatus == true
+    function updateStatus(string memory currentStatus) public returns (string memory) {
+        if (keccak256(bytes(currentStatus)) == keccak256(bytes("true")))
         {
             return "reachedLocation";
         }
