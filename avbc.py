069a21705092ac463b85745a36cfec02e106c176
Ritu Gala <ritugala13@gmail.com>
2020-08-21 18:23:27 +0530

    Update avbc.py

--- avbc.py
+++ avbc.py
@@ -1,7 +1,8 @@
 from web3 import Web3
 import json
 
-web3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+# Copy this part into your main file
+web3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545")) # Verify if your Ganache is at 7545 too, should be
 with open('contract1.json') as f:
     contractDetails = json.load(f)
     contractAddress = (contractDetails["address"])
@@ -9,21 +10,20 @@ with open('contract1.json') as f:
 
 Contract = web3.eth.contract(bytecode=contractAddress, abi=abi)
 
-_company = web3.eth.accounts[1]
-_cost = 1000000000000000000
-_lat = 10
-_long = 10
 
-tx_hash = Contract.constructor(_company=_company, _cost=_cost, _lat = _lat, _long = _long).transact(
-    transaction={'from': web3.eth.accounts[0], 'gas': 410000})
-
-# Get tx receipt to get contract address
-tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-contract_address = tx_receipt['contractAddress']
-contract = web3.eth.contract(
-    address=tx_receipt.contractAddress,
-    abi=abi,
-)
-
-# while input is received from sensors. for now values are hardcoded.
-contract.functions.updateStatus(10, 10).transact(transaction={'from': web3.eth.accounts[0], 'gas': 410000, 'value':10000000000000000000})
+def updateStatus(dest_lat, dest_long, curr_lat, curr_long):    
+    _company = web3.eth.accounts[1]
+    _cost = 1000000000000000000
+    _lat = dest_lat
+    _long = dest_long
+    
+    tx_hash = Contract.constructor(_company=_company, _cost=_cost, _lat = _lat, _long = _long).transact(
+        transaction={'from': web3.eth.accounts[0], 'gas': 410000})
+    
+    # Get tx receipt to get contract address
+    tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+    contract = web3.eth.contract(
+        address=tx_receipt.contractAddress,
+        abi=abi,
+    )    
+    contract.functions.updateStatus(curr_lat, curr_long).transact(transaction={'from': web3.eth.accounts[0], 'gas': 410000, 'value':10000000000000000000})
